9004515a22964ac89118696a552d57fd7d7ef516
Tamim-Amin <tamimamin13579@gmail.com>
2026-08-14 09:30:47 +0600

    feat: refactor MediBookApp to use MultiProvider for state management and remove local theme state

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'screens/dev/widget_gallery_screen.dart';
+import 'providers/auth_provider.dart';
+import 'providers/theme_provider.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/register_screen.dart';
+import 'screens/auth/welcome_screen.dart';
+import 'screens/main_shell.dart';
+import 'screens/onboarding/onboarding_screen.dart';
+import 'screens/splash_screen.dart';
 import 'utils/app_routes.dart';
 import 'utils/app_theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MediBookApp());
 }
 
-class MediBookApp extends StatefulWidget {
+class MediBookApp extends StatelessWidget {
   const MediBookApp({super.key});
 
   @override
-  State<MediBookApp> createState() => _MediBookAppState();
-}
-
-class _MediBookAppState extends State<MediBookApp> {
-  // TODO(Day 7): replace this local state with ThemeProvider + SharedPreferences.
-  ThemeMode _themeMode = ThemeMode.light;
-
-  void _toggleTheme() {
-    setState(() {
-      _themeMode =
-      _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MediBook',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: _themeMode,
-      initialRoute: AppRoutes.widgetGallery,
-      routes: <String, WidgetBuilder>{
-        // TODO(Day 3): remove the dev routes and register splash, onboarding,
-        // welcome, login, register and the main shell here.
-        AppRoutes.widgetGallery: (BuildContext context) =>
-            WidgetGalleryScreen(onToggleTheme: _toggleTheme),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
+        ChangeNotifierProvider<ThemeProvider>(create: (_) => ThemeProvider()),
+        // TODO(Day 4): DoctorProvider, FavoritesProvider
+        // TODO(Day 5): AppointmentProvider
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (BuildContext context, ThemeProvider theme, Widget? child) {
+          return MaterialApp(
+            title: 'MediBook',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: theme.themeMode,
+            initialRoute: AppRoutes.splash,
+            routes: <String, WidgetBuilder>{
+              AppRoutes.splash: (_) => const SplashScreen(),
+              AppRoutes.onboarding: (_) => const OnboardingScreen(),
+              AppRoutes.welcome: (_) => const WelcomeScreen(),
+              AppRoutes.login: (_) => const LoginScreen(),
+              AppRoutes.register: (_) => const RegisterScreen(),
+              AppRoutes.main: (_) => const MainShell(),
+              // TODO(Day 4): doctorList, doctorProfile, favorites
+              // TODO(Day 5): booking, bookingConfirmation
+              // TODO(Day 6): centerDetails
+              // TODO(Day 7): editProfile
+            },
+          );
+        },
+      ),
     );
   }
 }
