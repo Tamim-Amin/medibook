439ef16ec3d5aeb421e8c4336c93360cd3e2c959
Tamim-Amin <tamimamin13579@gmail.com>
2026-08-14 09:30:39 +0600

    feat: implement AppTextField widget with password toggle and customizable properties

---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../utils/app_text_styles.dart';
+import '../utils/context_colors.dart';
+
+/// A labelled [TextFormField] with consistent spacing and an optional
+/// show/hide toggle for passwords.
+class AppTextField extends StatefulWidget {
+  const AppTextField({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.hint,
+    this.validator,
+    this.keyboardType,
+    this.prefixIcon,
+    this.isPassword = false,
+    this.maxLines = 1,
+    this.maxLength,
+    this.inputFormatters,
+    this.textInputAction,
+    this.onFieldSubmitted,
+    this.enabled = true,
+  });
+
+  final String label;
+  final TextEditingController controller;
+  final String? hint;
+  final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
+  final IconData? prefixIcon;
+  final bool isPassword;
+  final int maxLines;
+  final int? maxLength;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextInputAction? textInputAction;
+  final void Function(String)? onFieldSubmitted;
+  final bool enabled;
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  late bool _obscure = widget.isPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            widget.label,
+            style: AppTextStyles.caption.copyWith(
+              color: context.cTextSecondary,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 7),
+          TextFormField(
+            controller: widget.controller,
+            validator: widget.validator,
+            keyboardType: widget.keyboardType,
+            obscureText: _obscure,
+            maxLines: widget.isPassword ? 1 : widget.maxLines,
+            maxLength: widget.maxLength,
+            inputFormatters: widget.inputFormatters,
+            textInputAction: widget.textInputAction,
+            onFieldSubmitted: widget.onFieldSubmitted,
+            enabled: widget.enabled,
+            style: AppTextStyles.body.copyWith(color: context.cTextPrimary),
+            decoration: InputDecoration(
+              hintText: widget.hint,
+              counterText: '',
+              prefixIcon: widget.prefixIcon == null
+                  ? null
+                  : Icon(widget.prefixIcon,
+                      size: 20, color: context.cTextSecondary),
+              suffixIcon: widget.isPassword
+                  ? IconButton(
+                      icon: Icon(
+                        _obscure
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        size: 20,
+                        color: context.cTextSecondary,
+                      ),
+                      onPressed: () => setState(() => _obscure = !_obscure),
+                    )
+                  : null,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
